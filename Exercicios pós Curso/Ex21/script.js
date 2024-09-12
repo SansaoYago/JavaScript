11632d0d8d71a44cac5e9b02ef9f02b4ad0ae010
Cristian Sansão <169001529+SansaoYago@users.noreply.github.com>
2024-09-12 19:42:05 -0300

    Começando Exercícios do Livro Lógica de Programação e Algoritmos com JavaScript

--- Exercicios pós Curso/Ex21/script.js
+++ Exercicios pós Curso/Ex21/script.js
@@ -2,8 +2,8 @@ function enviar() {
    let nome = document.querySelector("#nome")
    let resp = document.querySelector("#res")
    let preço = document.querySelector("#pre")
-   let des = ''
-   
+   let preco = Number(preço.value)
+
    if (nome.value.length == 0 || preço.value.length == 0) {
       window.alert("[ERRO] Faltam dados para concluir o processo!")
    } else {
@@ -12,17 +12,15 @@ function enviar() {
       let img = document.createElement('img')
       img.setAttribute('id', 'foto')
       if (sexo[0].checked) {
-         let des = (5 / 100) * preço.value
-         let preçoFinal = preço.value - des
+         let total = preco - (5 / 100 * preco)
          genero = 'Homem'
          img.setAttribute('src', 'imagens/homem.png')
-         resp.innerHTML = `Para ${genero} será aplicado o desconto de 5% <br> O cliente ${nome.value} vai pagar ${preçoFinal.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} <br>`
-      } else if (sexo[1].checked) { 
-         let des = (13 / 100) * preço.value
-         let preçoFinal = preço.value - des
+         resp.innerHTML = `Para ${genero} será aplicado o desconto de 5% <br> O cliente ${nome.value} vai pagar ${total.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} <br>`
+      } else if (sexo[1].checked) {  
+         let total = preco - (13 / 100 * preco)
          genero = 'Mulher'
          img.setAttribute('src', 'imagens/mulher.png')
-         resp.innerHTML = `Para ${genero} será aplicado o desconto de 13% <br> A cliente ${nome.value} vai pagar ${preçoFinal.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} <br>`
+         resp.innerHTML = `Para ${genero} será aplicado o desconto de 13%\nA cliente ${nome.value} vai pagar ${total.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} <br>`
       }
       resp.style.textAlign = 'center'
       resp.appendChild(img)
