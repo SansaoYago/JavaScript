78cad13dc6f79530b2ac514a72c5f35aef7e7857
Cristian Sansão <169001529+SansaoYago@users.noreply.github.com>
2024-09-23 19:45:07 -0300

    Ex4_3 do LivroJs

--- livroJs/cap04/Js/ex4_2.js
+++ livroJs/cap04/Js/ex4_2.js
@@ -8,13 +8,7 @@ frm.addEventListener("submit", (e) => {{
     const masculino = frm.inMasc.checked
     const altura = Number(frm.inAlt.value)
     
-    let peso 
-
-    if (masculino == true) {
-        peso = 22 * Math.pow(altura, 2)
-    } else {
-        peso = 21 * Math.pow(altura, 2)
-    }
+    const peso = masculino ? 22 * Math.pow(altura, 2): 21 * Math.pow(altura, 2)
 
     resp.textContent = `${nome}: Seu peso ideal é ${peso.toFixed(3)} kg`
 }})
