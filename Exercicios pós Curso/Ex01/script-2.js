ea8926ce0e64828aa4600544f405bb0d5de12c87
Cristian Sansão <169001529+SansaoYago@users.noreply.github.com>
2025-01-23 22:30:24 -0300

    Pequenas Alterações

--- Exercicios pós Curso/Ex01/script-2.js
+++ Exercicios pós Curso/Ex01/script-2.js
@@ -1,11 +1,19 @@
-let dado = window.document.querySelector("#dados")
-const resp = window.document.querySelector("res")
+const dado = window.document.querySelector("#dados")
+const resp = window.document.querySelector("#res")
 
 dado.addEventListener("click", (e) => {
     e.preventDefault()
 
-    let nome = prompt("Digite seu nome: ")
+    //Variáveis
+    let nome, div
     
+    //Entrada de dados
+    div = window.document.querySelector("#dados")
+    nome = prompt("Digite seu nome: ")
 
-    resp.textContent = `Seja bem vindo(a) ${nome}`
+    //Processamento de dados
+    dado.style.display = 'none'
+
+    //Saída de dados
+    resp.innerHTML = `Seja bem vindo(a) ${nome}`
 })
