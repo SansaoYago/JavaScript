45aa4939ce6da1cf788a894d264f1238832cbcff
Cristian Sansão <169001529+SansaoYago@users.noreply.github.com>
2026-02-05 18:25:23 -0300

    Iniciando EX02

---
+++ pages/livroJS/cap06/js/Ex02.js
@@ -0,0 +1,14 @@
+const form = document.querySelector("form")
+const respErros = document.querySelector(".erros span")
+const chaces = document.querySelector(".chances span")
+const dica = document.querySelector(".dica")
+
+const erros = []
+const sort = Math.floor(Math.random() * 100) + 1
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+
+    
+})
