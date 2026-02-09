c066add16ca1563208f764ae67f617243c5811d0
Cristian Sansão <169001529+SansaoYago@users.noreply.github.com>
2026-02-09 08:27:00 -0300

    Finalizando ex02

--- pages/livroJS/cap06/js/Ex02.js
+++ pages/livroJS/cap06/js/Ex02.js
@@ -1,27 +1,46 @@
 const form = document.querySelector("form")
-const respErros = document.querySelector("#erros")
-const chances = document.querySelector("#chances")
-const dica = document.querySelector(".dica")
+const respErros = document.querySelector("#outErros")
+const respChances = document.querySelector("#outChances")
+const respDica = document.querySelector("#outDica")
 
 const erros = []
 const sort = Math.floor(Math.random() * 100) + 1
-const nChances = 6
+const CHANCES = 6
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
-    alert(sort)
+
     const numero = Number(form.inNumero.value)
     
     if (numero == sort) {
         dica.innerHTML = `Parabéns número sorteado ${sort}`
         form.btSubmit.disabled = true
         form.btNovo.classList.add = "exibe"
+    } else {
+        if (erros.includes(numero)) {
+            alert(`Você já apostou o número ${numero}. Tente outro...`)
+        } else {
+            erros.push(numero)
+            const nErros = erros.length
+            const nChances = CHANCES - nErros
+
+            respErros.innerHTML = `${nErros} (${erros.join(", ")})`
+            respChances.innerHTML = nChances
+            if (nChances == 0) {
+            alert("Suas chances acabaram...")
+            form.btSubmit.disabled = true
+            form.btNovo.className = "exibe"
+            respDica.textContent = `Game over!! Número sorteado ${sort}`
+        } else {
+            const dica = numero < sort ? "maior" : "menor"
+            respDica.textContent = `Dica: Tente um número ${dica} que ${numero}`
+        }
+        }
+        
+        
     }
 
 
-
-    respErros.innerHTML = erros
-    chances.innerHTML = nChances
     form.inNumero.value = ""
     form.inNumero.focus()
     
