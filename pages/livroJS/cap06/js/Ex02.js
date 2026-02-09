7faa6d6b3d0f94d0a59bb18b6f64d53750ceb877
Cristian Sansão <cristian.syb@gmail.com>
2026-02-08 22:04:09 -0300

    Iniciando exercício 02
    Jogo descubra o número

--- pages/livroJS/cap06/js/Ex02.js
+++ pages/livroJS/cap06/js/Ex02.js
@@ -1,14 +1,28 @@
 const form = document.querySelector("form")
-const respErros = document.querySelector(".erros span")
-const chaces = document.querySelector(".chances span")
+const respErros = document.querySelector("#erros")
+const chances = document.querySelector("#chances")
 const dica = document.querySelector(".dica")
 
 const erros = []
 const sort = Math.floor(Math.random() * 100) + 1
+const nChances = 6
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
+    alert(sort)
+    const numero = Number(form.inNumero.value)
+    
+    if (numero == sort) {
+        dica.innerHTML = `Parabéns número sorteado ${sort}`
+        form.btSubmit.disabled = true
+        form.btNovo.classList.add = "exibe"
+    }
 
 
+
+    respErros.innerHTML = erros
+    chances.innerHTML = nChances
+    form.inNumero.value = ""
+    form.inNumero.focus()
     
 })
