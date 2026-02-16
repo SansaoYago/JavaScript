51396741454d0a3a8f9b9f2d93e88605adc17a6d
Cristian Sansão <169001529+SansaoYago@users.noreply.github.com>
2026-02-16 18:47:54 -0300

    Treinando reduce para reduzir listas e contar as duplicidades de dados

--- DESAFIOS/js/desafio03.js
+++ DESAFIOS/js/desafio03.js
@@ -1,5 +1,13 @@
 const button = document.querySelector("input")
+const out = document.querySelector("#out1")
+const nomes = ["Ana", "Beto", "Ana", "Caio", "Beto", "Ana"]
 
 button.addEventListener("click", () => {
-    
+    const contagem = nomes
+        .reduce((cont, nomeAtual) => {
+            cont[nomeAtual] = (cont[nomeAtual] || 0) + 1
+            return cont
+        }, {})
+
+    out.textContent = JSON.stringify(contagem, null, 2)
 })
